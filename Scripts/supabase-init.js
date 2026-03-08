dbbe6f5a22c7d5b254f4c477339fc0c070e472c6
Milap Khasi <mkdevelopers2201@gmail.com>
2026-03-08 10:36:15 +0530

    Deleting everything

--- Scripts/supabase-init.js
+++
@@ -1,7 +0,0 @@
-const supabaseUrl = 'https://rfkitszqowshaoqkltit.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJma2l0c3pxb3dzaGFvcWtsdGl0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI1NDMwODUsImV4cCI6MjA4ODExOTA4NX0.OOzrIvJTEoJ0De3eVD7qCaouMyl6e_Er-bY5tAUXrH8';
-
-const { createClient } = supabase;
-window.supabase = createClient(supabaseUrl, supabaseKey);
-
-console.log("Supabase initialized successfully.");
